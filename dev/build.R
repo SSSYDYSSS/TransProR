b1e8d6197b4cfa3a1c8290357fd684edbffd4101
SSSYDYSSS <yudongyue@mail.nankai.edu.cn>
2024-04-25 13:06:41 +0800

    somechange

--- dev/build.R
+++ dev/build.R
@@ -4,6 +4,9 @@ usethis::use_pipe(export = TRUE)
 ## code to prepare MAKEDATA.R
 usethis::use_data_raw("MAKEDATA")
 
-devtools::load_all()
+
 devtools::document()
 devtools::check()
+
+
+devtools::load_all()
