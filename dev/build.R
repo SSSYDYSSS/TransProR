d957959b7c40ebcb5f032e413815ce818513c23e
SSSYDYSSS <yudongyue@mail.nankai.edu.cn>
2024-04-25 15:52:59 +0800

    somechange

--- dev/build.R
+++ dev/build.R
@@ -9,3 +9,9 @@ devtools::load_all()
 
 devtools::document()
 devtools::check()
+
+
+# Ensure that all dependencies are correctly installed, and generate the pak lock file.
+pak::pak()
+pak::lockfile_create()# for github action
+file.rename("pkg.lock", ".github/pkg.lock")
